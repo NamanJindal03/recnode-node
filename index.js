1dfb30effcd20e9ab0bb36ef5c49192901895628
Naman Jindal <namanjindal113@gmail.com>
2024-05-08 22:39:48 +0530

    middleware part 2

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.get('/middleware-usage',
         req.manipulated = true;
         next();
     }, 
+    (req, res, next) => {
+        //middleware -> manipulate the req
+        console.log('I am  middleware 2' );
+        //some more manipulation done 
+        next();
+    }, 
+    (req, res, next) => {
+        //middleware -> manipulate the req
+        console.log('I am  middleware 3' );
+        //some more manipulation done 
+        next();
+    }, 
     (req, res)=>{
         const didMiddlewareActuallyWork = req.manipulated;
         console.log(JSON.stringify(didMiddlewareActuallyWork));
@@ -34,6 +46,87 @@ app.get('/middleware-usage',
         res.send('Actual logic returned.')
     }
 )
+
+function one(req, res, next){
+    //middleware -> manipulate the req
+    console.log('I am currently in the middleware');
+    req.manipulated = true;
+    next();
+}
+function two(req, res, next){
+    //middleware -> manipulate the req
+    console.log('I am  middleware 2' );
+    //some more manipulation done 
+    next();
+}
+function three(req, res, next){
+    //middleware -> manipulate the req
+    console.log('I am  middleware 3' );
+    //some more manipulation done 
+    next();
+}
+
+app.get('/middleware-usage-2',one,two,three, (req, res)=>{
+        const didMiddlewareActuallyWork = req.manipulated;
+        console.log(JSON.stringify(didMiddlewareActuallyWork));
+        //actual logic
+        console.log('I am the actual logic');
+        res.send('Actual logic returned.')
+    }
+)
+
+function timeAppendMiddleware(req, res, next){
+    req.timeOfExec = Date.now();
+    next();
+}
+
+app.get('/one', timeAppendMiddleware, (req, res) => {
+    console.log('final');
+    res.json({time: req.timeOfExec})
+})
+
+app.get('/two', timeAppendMiddleware, (req, res) => {
+    console.log('final 2');
+    res.json({time: req.timeOfExec})
+})
+
+app.get('/three', timeAppendMiddleware, (req, res) => {
+    console.log('final 3');
+    res.json({time: req.timeOfExec})
+})
+
+function isSeller(req, res, next){
+    const userId = req.params.userId;
+    if(userId%2 !== 0){
+        return res.status(403).json({error: 'not authorized'})
+    }
+    next();
+}
+function isCustomer(){
+    //code logic
+}
+
+///segregating seller and customer 
+app.get('/one/:userId', isSeller, (req, res) => {
+    console.log('final');
+    res.json({time: req.timeOfExec})
+})
+
+app.get('/two/:userId',isCustomer, (req, res) => {
+    console.log('final 2');
+    res.json({time: req.timeOfExec})
+})
+
+app.get('/three/:userId', isSeller, (req, res) => {
+    console.log('final 3');
+    res.json({time: req.timeOfExec})
+})
+
+
+
+
+
+
 
 app.listen(PORT, ()=>{
     // console.log(process)
