48d84e6fcf9fb3e6d0f906958ae7437783bc0f3e
Naman Jindal <namanjindal113@gmail.com>
2024-05-13 23:01:23 +0530

    progress 2

--- may13th.js
+++ may13th.js
@@ -16,6 +16,50 @@ app.get('/products', async (req, res) => {
     // console.log(data);
 })
 
+app.post('/products', async (req, res) => {
+    const productData = req.body;
+    let isValid = true;
+    const schemaValidProduct = productData.map((product)=>{
+        const {id, productName, price} = product;
+        if(!id || !productName || !price){
+            isValid = false;
+            return undefined;
+        }
+        return {id, productName, price}
+    })
+    if(!isValid){
+        return res.status(400).json({error: 'product schema not correct'})
+    }
+
+    try{
+        await fs.writeFile(path.join(__dirname, PRODUCT_FILE_NAME), JSON.stringify(schemaValidProduct));
+        return res.status(200).json({data: schemaValidProduct, message: 'products overwritten succesfully'})
+    }
+    catch(err){
+        return res.status(400).json({error: err})
+    }
+})
+
+app.put('/products', async (req, res) => {
+    const {id, productName, price} = req.body;
+    const newProduct = {id, productName, price}
+    try{
+        const data = await fs.readFile(path.join(__dirname, PRODUCT_FILE_NAME));
+        let dataArr = JSON.parse(data);
+        const isDuplicate = dataArr.find((product)=> product.id === newProduct.id);
+        if(isDuplicate){
+            return res.status(400).json({error: 'product already exists'})  
+        }
+        dataArr.push(newProduct);
+        await fs.writeFile(path.join(__dirname, PRODUCT_FILE_NAME), JSON.stringify(dataArr));
+        return res.status(200).json({data: newProduct, message: 'product added'})
+    }
+    catch(err){
+        return res.status(400).json({error: err})
+    }
+
+})
+
 
 app.listen(PORT, ()=>{
     // console.log(process)
