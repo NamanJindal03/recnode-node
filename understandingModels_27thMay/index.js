148e0ba0f369ca50567545e0ce13cc71e8491a8c
Naman Jindal <namanjindal113@gmail.com>
2024-05-30 22:48:19 +0530

    base template added

--- understandingModels_27thMay/index.js
+++ understandingModels_27thMay/index.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const User = require('./models/user.models')
 
 mongoose.connect("mongodb://localhost/t1")
-
-const user1 = new User({rollNumber: 20, name: 'umang', email: 'namanjindal99@gmail.com'});
+    .then(()=> console.log('db succesfully connected'))
+    .catch((err) => console.log(err))
+const user1 = new User({rollNumber: 20, name: 'umang', email: 'namanjindal991@gmail.com', age: 18});
 user1.save().then(()=>{console.log('user created')});
 
+/*
+    async function sample(){
+        await test();
+        console.log('testing completed')
+    }
+*/
--- understandingModels_27thMay/models/user.models.js
+++ understandingModels_27thMay/models/user.models.js
@@ -9,28 +9,37 @@ const mongoose = require('mongoose');
 //     isFemale: Boolean,
 // })
 
-const userSchema = new mongoose.Schema({
-    rollNumber: {
-        //predefined key
-        type: Number,
-        required: true
-    },
-    name: {
-        type: String, 
-        required: true
-    },
-    status: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String, 
-        required: true,
-        unique: true
-    }
+const userSchema = new mongoose.Schema(
+    {
+        rollNumber: {
+            //predefined key
+            type: Number,
+            required: true
+        },
+        name: {
+            type: String, 
+            required: true
+        },
+        status: {
+            type: String,
+            required: false
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true
+        },
+        age: {
+            type: Number,
+            required: true,
+            min: 18,
+            max: 150
+        }   
 
     //name, isFemale, status, email
-})
+    },
+    {timestamps: true}
+)
 
 module.exports = mongoose.model('User', userSchema );
 
