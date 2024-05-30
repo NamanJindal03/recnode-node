1380292ff739f2dcc5dbfc8090d10d6010a399e9
Naman Jindal <namanjindal113@gmail.com>
2024-05-30 23:15:49 +0530

    full mvc flow

--- folderStructure2/src/controllers/user.controller.js
+++ folderStructure2/src/controllers/user.controller.js
@@ -1,8 +1,16 @@
 const User = require('../models/user.models');
 
-function createUser(req, res){
+async function createUser(req, res){
+    const {rollNumber, name, status, email,age } = req.body
     //work with your User model
-    console.log('trial');
-    res.send('working')
+    try{
+        const newUser = new User({rollNumber, name, status, email, age});
+        const result = await newUser.save()
+        console.log('user saved');
+        res.status(201).json({message: 'user created successfully', status: 'success', user: result})
+    }
+    catch(err){
+        res.status(404).json({message: 'something went wrong', status: 'failure', error: err})
+    }
 }
 module.exports = {createUser}
